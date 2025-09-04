1eaedfb5f8332a9e522eb4d799aef1ed2d0ad675
Jhoseph <jhoseph@Jhoseph.Jhoseph>
2025-09-03 22:21:50 -0400

    add reto-8

---
+++ retos_02/ejercicio_01.py
@@ -0,0 +1,14 @@
+numero1 = input("ingresa el primer numero")
+numero2 = input("ingresa el segundo numero")
+numero1 = int(numero1)
+numero2 = int(numero2)
+suma= numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print("Suma: ",suma)
+print("Resta: ",resta)
+print("Multiplicacion: ", multiplicacion)
+print("Division: ", division)
+
