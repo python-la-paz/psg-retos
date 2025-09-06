a45d88a96ded99b57261b7143bd34a98776ddfa7
Daniel Rios <daniel.ids.1.3@gmail.com>
2025-09-05 21:05:08 -0400

    Reto 08 completado - tipos de datos

---
+++ retos_02/ejercicio_01.py
@@ -0,0 +1,15 @@
+print("Ingrese el primer numero: ")
+num1 = int(input())
+
+print("Ingrese el segundo numero que sea distinto de 0")
+num2 = int(input())
+
+suma = num1 + num2
+resta = num1 - num2
+multiplicacion = num1 * num2
+division = num1 / num2
+
+print("Suma: " + str(suma))
+print("Resta: " + str(resta))
+print("Multiplicación: " + str(multiplicacion))
+print("División: " + str(division))
