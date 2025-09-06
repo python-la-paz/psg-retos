d431491a323a1cc17013fb2d3fb65021e03c5b55
Valeria Fernandez <valeria.fernalva@gmail.com>
2025-09-06 00:07:08 -0400

    ejercicio_02.py

--- retos_02/ejercicio_02.py
+++ retos_02/ejercicio_02.py
@@ -1,11 +1,11 @@
 ## 2.
 
-estado_de_sistema_de_riego=("El sistema de riego no se activa.","El sistema de riego se activa.")
+
 #---
 luz_solar=eval(input("Luz solar (True/False): "))
 humedad=int(input("Humedad: "))
 #--
 factor_humedad=humedad<30
-sistema_de_riego=luz_solar!=(humedad<30)
+sistema_de_riego=luz_solar!=(factor_humedad)
 #---
-print(estado_de_sistema_de_riego[sistema_de_riego])
+print(("El sistema de riego se activa." * sistema_de_riego) + ("El sistema de riego no se activa." * (not sistema_de_riego)))
